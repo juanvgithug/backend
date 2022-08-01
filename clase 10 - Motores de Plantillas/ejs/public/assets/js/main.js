88f5031be1238a72bac9b88d4b4fc65b3ce36150
Juan Vidal <vidalbagnoli@gmail.com>
2022-08-01 12:34:47 -0300

    Clase 10 - Motores de Plantillas

---
+++ clase 10 - Motores de Plantillas/ejs/public/assets/js/main.js
@@ -0,0 +1,103 @@
+/*
+
+Curso : BackEnd
+Alumno : Juan Vidal 
+Comisión:  31030
+
+*/
+
+// General 
+function fswal(title, text) {
+    Swal.fire(title, text, "info");
+}
+
+//fButtonHandler
+function fButtonHandler(nButton) {
+    console.log("boton", nButton)
+    switch (nButton) {
+
+        case 1: //get all elements
+            // JSON result in `data` variable
+            fetch('/api/productos')
+                .then(res => res.json(), {
+                    mode: "no-cors"
+                }) // the .json() method parses the JSON response into a JS object literal
+                .then(
+                    function (data) {
+                        console.log(data);
+                        let messages = '<div class="ResponseText">';
+                        $.each(data, function (index, record) {
+                            // $.each(record, function (index, record) {
+                                for (var property in record) {
+                                    if (record.hasOwnProperty(property)) {
+                                        messages += '<b>' + property + '</b>: ' + record[property] + '<br>';
+                                    }
+                                }
+                                messages += '<br>';
+                            // })
+                        })
+                        messages += '</div>';
+                        fswal("Respuesta de /api/productos", messages);
+                    });
+            break;
+
+        case 2: //POST new product
+            //window.location.href = '/productoRandom';
+            Swal.fire({
+            title: 'Alta de Producto',
+                html: `
+                    <input type="text" id="title" class="swal2-input" placeholder="Mi Producto">
+                    <input type="text" id="price" class="swal2-input" placeholder="123.45">
+                    <input type="text" id="thumbnail" class="swal2-input" placeholder="http://imagenes.com/miImagen.jpg">
+                  `,
+            confirmButtonText: 'Enviar',
+            focusConfirm: false,
+            preConfirm: () => {
+                const title = Swal.getPopup().querySelector('#title').value
+                const price = Swal.getPopup().querySelector('#price').value
+                const thumbnail = Swal.getPopup().querySelector('#thumbnail').value
+                if (!title || !price || !thumbnail) {
+                Swal.showValidationMessage(`Debe completar todos los datos`)
+                }
+                return { title: title, price: price, thumbnail: thumbnail }
+            }
+            })
+                .then((result) => {
+                    let prod = {
+                        title: result.value.title,
+                        price: result.value.price,
+                        thumbnail: result.value.thumbnail,
+                    };
+                    /*let response = */
+                    fetch('/api/productos/', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json;charset=utf-8'
+                        },
+                        body: JSON.stringify(prod)
+                    })
+                    .then(res => res.json(), {
+                        mode: "no-cors"
+                    }) // the .json() method parses the JSON response into a JS object literal
+                    .then(
+                        function (data) {
+                            let messages = '<div class="ResponseText">';
+                            messages += '<b>' + "Title" + '</b>: ' + data.title + '<br>';
+                            messages += '<b>' + "Price" + '</b>: ' + data.price + '<br>';
+                            messages += '<b>' + "Thumbnail" + '</b>: ' + data.thumbnail + '<br>';
+                            messages += '<b>' + "ID" + '</b>: ' + data.id + '<br>';
+                            messages += '</div>';
+                            fswal("Respuesta obtenida", messages);
+                    })                    
+                });
+            break;
+        default:
+            break;
+    }
+}
+
+// DOM Ready
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log("DOM Loaded: OK");
+
+});
---
+++ clase 10 - Motores de Plantillas/pug/src/server.js
@@ -0,0 +1,74 @@
+// Alumno : Juan Vidal - Comisión 31030
+// Desafio Clase 10 - Motores de plantillas
+
+const chalk = require("chalk");
+const toolBox = require('../util/toolBox');
+const datosEntregable = "Desafio: Clase Clase 10 - Motores de plantillas"
+const express = require('express');
+const Productos = require('../api/productos.js');
+const productos = new Productos();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
+
+//--------------------------------------------
+app.set('views', './views');
+app.set('view engine', 'pug');
+//--------------------------------------------
+
+app.post('/productos', (req, res) => {
+  const producto = req.body
+  productos.guardar(producto)
+  res.redirect('/')
+})
+
+app.get('/productos', (req, res) => {
+  const prods = productos.listarAll()
+
+  res.render("vista", {
+    productos: prods,
+    hayProductos: prods.length
+  });
+});
+
+//--------------------------------------------
+
+app.post('/productos', (req, res) => {
+  const producto = req.body
+  productos.guardar(producto)
+  res.redirect('/')
+})
+app.get('/productos', (req, res) => {
+  const prods = productos.listarAll()
+  res.render("vista", {
+    productos: prods,
+    hayProductos: prods.length,
+    datosEntregable: datosEntregable
+  });
+});
+
+console.clear();
+toolBox.sayHi();
+
+const PORT = 8989;
+const server = app.listen(PORT, () => {
+  console.log(chalk.green(`Server Up en puerto ${server.address().port}`));
+});
+
+server.on('error', (err) => {
+  toolBox.sayError(err);
+});
+
+app.get("/about", (req, res) => {
+  console.clear();
+  toolBox.sayHi();
+  console.log(chalk.yellow(`Request from: ${req.headers.host}${req.url} `));
+  res.json({
+    message: "Bienvenido a la api de Productos."
+  });
+  console.log(chalk.green(`-> Sent greeting.`));
+});
